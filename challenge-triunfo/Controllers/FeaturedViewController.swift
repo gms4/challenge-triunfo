51ea6e749001e184880cdc52eecaf2a74a3966df
gms4 <gms4@cin.ufpe.br>
2022-07-11 17:08:29 -0300

    finalizando aula 4

--- challenge-triunfo/Controllers/FeaturedViewController.swift
+++ challenge-triunfo/Controllers/FeaturedViewController.swift
@@ -7,14 +7,32 @@
 
 import UIKit
 
-class FeaturedViewController: UIViewController {
+class FeaturedViewController: UIViewController, UICollectionViewDataSource {
 
+    @IBOutlet var popularCollectionView: UICollectionView!
+    @IBOutlet var nowPlayingCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        popularCollectionView.dataSource = self
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "popularCell", for: indexPath) as? PopularCollectionViewCell {
+            cell.titleLabel.text = "Título do Filme"
+            cell.image.image = UIImage()
+            return cell
+        }
+        
+        return UICollectionViewCell()
+    
     }
 
-    @IBOutlet var popularCollectionView: UICollectionView!
-    
 }
-
